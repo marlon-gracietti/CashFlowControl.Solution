aa9278d1ee64f8a38620cd1dddba1e62453ea7b1
Marlon Gracietti de Amorim <marlon@devpraxis.com.br>
2024-08-04 17:07:58 -0300

    Squashed 'CashFlowControl.DailySummaryService/' content from commit 32a87a6
    git-subtree-dir: CashFlowControl.DailySummaryService
    git-subtree-split: 32a87a666f07bfe99ab40052ecf4f31961fa9bc4

---
+++ DailySummaryService/Controllers/DailySummaryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CashFlowControl.Application.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace CashFlowControl.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DailySummaryController : ControllerBase
+    {
+        private readonly DailySummaryService _dailySummaryService;
+
+        public DailySummaryController(DailySummaryService dailySummaryService)
+        {
+            _dailySummaryService = dailySummaryService;
+        }
+
+        // GET: api/DailySummary/{date}
+        [HttpGet("{date}")]
+        public async Task<IActionResult> GetDailySummary(DateTime date)
+        {
+            var summary = await _dailySummaryService.GetDailySummary(date);
+            if (summary == null)
+            {
+                return NotFound();
+            }
+            return Ok(summary);
+        }
+    }
+}
