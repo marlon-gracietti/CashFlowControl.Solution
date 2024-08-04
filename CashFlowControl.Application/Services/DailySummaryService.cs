8ceca8186d274778d9a8ca5b709147a2a7917450
Marlon Gracietti de Amorim <marlon@devpraxis.com.br>
2024-08-04 17:07:22 -0300

    Squashed 'CashFlowControl.Shared/' content from commit f6ebcfe
    git-subtree-dir: CashFlowControl.Shared
    git-subtree-split: f6ebcfee7308eaa1445de1add6e15cfb78447437

---
+++ CashFlowControl.Application/Services/DailySummaryService.cs
@@ -0,0 +1,38 @@
+﻿using CashFlowControl.Core.Entities;
+using CashFlowControl.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashFlowControl.Application.Services
+{
+    public class DailySummaryService
+    {
+        private readonly CashFlowContext _context;
+
+        public DailySummaryService(CashFlowContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<DailySummary> GetDailySummary(DateTime date)
+        {
+            var transactions = await _context.Transactions
+                .Where(t => t.Date.Date == date.Date)
+                .ToListAsync();
+
+            var totalCredits = transactions.Where(t => t.IsCredit).Sum(t => t.Amount);
+            var totalDebits = transactions.Where(t => !t.IsCredit).Sum(t => t.Amount);
+            var balance = totalCredits - totalDebits;
+
+            return new DailySummary
+            {
+                Date = date,
+                TotalCredits = totalCredits,
+                TotalDebits = totalDebits,
+                Balance = balance
+            };
+        }
+    }
+}
---
+++ CashFlowControl.Core/Entities/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace CashFlowControl.Core.Entities
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public bool IsCredit { get; set; }
+    }
+}
