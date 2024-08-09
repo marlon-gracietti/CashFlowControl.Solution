fae2915f0cbdd9e28aa6a209b04096578335cf65
Marlon Gracietti de Amorim <marlon@devpraxis.com.br>
2024-08-09 19:40:41 -0300

    Squashed 'path/to/subtree/' content from commit f512cbc
    git-subtree-dir: path/to/subtree
    git-subtree-split: f512cbcaf6b61a0cbdb5a0654e724cab182c1e04

---
+++ CashFlowControl.Core/Entities/DailySummary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CashFlowControl.Core.Entities
+{
+    public class DailySummary
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalCredits { get; set; }
+        public decimal TotalDebits { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
---
+++ CashFlowControl.Infrastructure/Migrations/20240524230413_CreateTransactionsTable.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace CashFlowControl.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTransactionsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "DailySummaries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    TotalCredits = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TotalDebits = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Balance = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DailySummaries", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Description = table.Column<string>(type: "longtext", nullable: false),
+                    IsCredit = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DailySummaries");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
